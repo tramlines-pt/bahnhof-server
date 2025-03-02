8df12e6304a5d6a3d13b59a1ca8f618aff14e979
Joshua Jun <joshuajun@proton.me>
2025-03-02 02:39:38 +0100

    add duration parameter to current endpoint

--- src/endpoints/current.js
+++ src/endpoints/current.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 router.get('/:evaNo', async (req, res) => {
     const evaNo = req.params.evaNo;
+    const duration = req.query.duration || 360;
 
     try {
         //example url: https://www.bahnhof.de/api/boards/departures?evaNumbers=8000105&filterTransports=BUS&duration=60&locale=de
-        const planResponse = await axios.get(`https://www.bahnhof.de/api/boards/departures?evaNumbers=${evaNo}&duration=360&locale=de`);
+        const planResponse = await axios.get(`https://www.bahnhof.de/api/boards/departures?evaNumbers=${evaNo}&duration=${duration}&locale=de`);
 
         res.json(planResponse.data);
     } catch (error) {
